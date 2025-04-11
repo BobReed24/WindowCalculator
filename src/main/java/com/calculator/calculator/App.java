9dff63cf18aab7d93f05f665ceac88f094a85428
Bob Reed <alex@ayxia.com>
2025-04-11 12:54:56 +0000

    Update

---
+++ src/main/java/com/calculator/calculator/App.java
@@ -0,0 +1,10 @@
+package com.calculator;
+
+import com.calculator.calculator.ui.CalculatorUI;
+
+public class App {
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
+}
